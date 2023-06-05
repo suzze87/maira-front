fcfe9b5ff95c3486ea2755c6308ef9e05bb9f5ff
suzze87 <suzzenelson@gmail.com>
2023-06-05 04:19:39 -0300

    no terminada mensaje 2

--- src/pages/Admin/AdminQuestions.tsx
+++ src/pages/Admin/AdminQuestions.tsx
@@ -42,7 +42,7 @@ const AdminQuestions: React.FC = () => {
   return (
     <>
       <div>
-        <pre>(maira) Esta parte no esta terminada!!</pre>
+        <pre className="mt-12 mb-12">(maira) Esta parte no esta terminada!!</pre>
         {questions && questions.length && questions.length >= 1
           ? questions.map((question: any) => (
               <Collapsible key={question.id} trigger={`Pregunta ID: #${question.id}`}>
